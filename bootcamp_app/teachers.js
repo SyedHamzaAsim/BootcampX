1d1af8ccd5cfc9a9cac3892f96d5035110400960
SyedHamzaAsim <hamza.asim090@gmail.com>
2022-07-01 06:36:49 +0000

    Satinize user inputs

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`]
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests on teachers.id = teacher_id
+JOIN students on student_id = students.id
+JOIN cohorts on cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+`;
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
